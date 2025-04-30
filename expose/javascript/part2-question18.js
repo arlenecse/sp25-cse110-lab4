a448ffa3322db9982c669243e2c74f17f174fc80
Arlene Garcia <146496596+arlenecse@users.noreply.github.com>
2025-04-29 22:13:55 -0700

    Update part2-question18.js

--- expose/javascript/part2-question18.js
+++ expose/javascript/part2-question18.js
@@ -1,5 +1,8 @@
-// Modify code so that it prints out time onces execueted
+// Modify code so that it prints out current time every second
+function currentTime() {    
+  let d = new Date();
+  let time = d.toLocaleTimeString();
+  console.log(time);
+}
 
-let d = new Date();
-let time = d.toLocaleTimeString();
-console.log(time);
+setInterval(currentTime, 1000);
